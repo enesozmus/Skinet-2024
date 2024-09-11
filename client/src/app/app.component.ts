09006ad8c0e46cd774750d7cbd1d02aea9ed4018
enesozmus <ensozmus@gmail.com>
2024-09-11 16:39:25 +0300

    Making http requests and Using types in our project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { HeaderComponent } from "./layout/header/header.component";
+import { Pagination } from './shared/models/pagination';
+import { Product } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,21 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Skinet-2024';
+export class AppComponent implements OnInit {
+
+  title: string = 'Skinet-2024';
+  baseURL: string = "https://localhost:5001/api/";
+  products: Product[] = [];
+  
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http
+          .get<Pagination<Product>>(this.baseURL + 'products')
+          .subscribe({
+            next: response => this.products = response.data,
+            error: error => console.log(error),
+            complete: () => console.log('complete')
+          })
+  }
 }
